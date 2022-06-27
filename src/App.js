1a8fde9b4213e98a02d3f3642659029df2e6cd7b
jacerchetoui56 <77737324+jacerchetoui56@users.noreply.github.com>
2022-06-27 19:16:58 +0100

    adding the main files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react"
+import Task from './Task'
+import './styles.css'
 function App() {
+
+  const [tasks, setTasks] = useState(
+    [{
+      name: 'this is the first task',
+      isDone: false
+    }
+    ])
+  const [form, setForm] = useState('')
+
+
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setTasks(prev => {
+      return [
+        ...prev,
+        {
+          name: form,
+          isDOne: false
+        }
+      ]
+    })
+    setForm('')
+  }
+
+  function taskDone(index) {
+    const newTasks = [...tasks]
+    newTasks[index].isDone = true
+    setTasks(newTasks)
+  }
+  function deleteTask(index) {
+    const newTasks = [...tasks]
+    newTasks.splice(index, 1)
+    setTasks(newTasks)
+  }
+
+  function modifyTask(index) {
+    console.log("modified")
+    setForm(tasks[index].name)
+    deleteTask(index)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className="todos">Todos ({tasks.filter(task => !task.isDone).length})</div>
+      <form className="form" onSubmit={handleSubmit} >
+        <input
+          type="text"
+          placeholder="Enter a task"
+          name="form"
+          value={form}
+          onChange={(e) => setForm(e.target.value)}
+        />
+        <button >Add Task</button>
+      </form>
+
+      <div className="tasks">
+        {tasks.map((task, index) => {
+          return <Task
+            infos={task}
+            handleDone={() => taskDone(index)}
+            handleDelete={() => deleteTask(index)}
+            handleModify={() => modifyTask(index)}
+          />
+        })}
+      </div>
     </div>
   );
 }
